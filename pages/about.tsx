4c499a8d7285e7ea4241daff3e9beb2fa88970d1
unknown <ajaydkrish007@gmail.com>
2023-04-06 00:00:26 +0530

    about us

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,21 @@
-import Breadcrumbs, { BreadCrumbsProps } from "@/components/common/breadcrumbs"
-import { useState } from "react"
+import AboutMainScreen from "@/components/about/about_main_screen";
+import Breadcrumbs, { BreadCrumbsProps } from "@/components/common/breadcrumbs";
+import { useState } from "react";
 
 export default function AboutPage() {
-    const [breadCrumbsProps, setBreadCrumbsProps] = useState<BreadCrumbsProps>(new BreadCrumbsProps({ backgroundColor: "#2e279d", backgroundImage: "assets/images/breadcrum-background.jpg", title: "About Us", textColor: "white", previous: { name: "Home", route: "/home" }, }))
-    return (
+  const [breadCrumbsProps, setBreadCrumbsProps] = useState<BreadCrumbsProps>(
+    new BreadCrumbsProps({
+      backgroundColor: "#2e279d",
+      backgroundImage: "assets/images/breadcrum-background.jpg",
+      title: "About Us",
+      textColor: "white",
+      previous: { name: "Home", route: "/home" },
+    })
+  );
+  return (
     <div className="">
-            <Breadcrumbs {...breadCrumbsProps} />
+      <Breadcrumbs {...breadCrumbsProps} />
+      <AboutMainScreen/>
     </div>
-    )
-  }
+  );
+}
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,12 +1,13 @@
+import AboutMainScreen from "@/components/about/about_main_screen";
 import Appointment from "@/components/home/appointment";
 import WhyChooseUs from "@/components/home/why_choose_us";
 
 export default function HomePage() {
-    return (
-        <div className="">
-            <Appointment />
-            <WhyChooseUs />
-        </div>
-
-    )
+  return (
+    <div className="">
+      <Appointment />
+      <WhyChooseUs />
+      <AboutMainScreen />
+    </div>
+  );
 }
